34c5b20541ca4a8938909e062b69d927a39c7a6f
Firstname Lastname <youcef.boucheta.simplon@gmail.com>
2023-01-29 12:11:36 +0100

    New app

--- app/app.py
+++ app/app.py
@@ -43,10 +43,10 @@ def check_password():
     if "password_correct" not in st.session_state:
         # First run, show input for password.
         st.text_input(
-            "Login", on_change=password_entered, key="login"
+            "Login", on_change=password_entered, key="youcef"
         )
         st.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
+            "Password", type="password", on_change=password_entered, key="boucheta"
         )
         return False
     elif not st.session_state["password_correct"]:
